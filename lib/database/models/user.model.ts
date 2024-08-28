9fbc51970a180869d20ad66c3f455b250fd860cc
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-08-28 23:09:29 +0530

    webhook

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,36 +1,40 @@
-import { model, models, Schema } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    username: {
-        
-    },
-    firstName: {
-        type: String,
-    },
-    lastName: {
-        type: String,
-    },
-    photo: {
-        type: String,
-    },
-    planId: {
-        type: Number,
-    },
-    creditBalance: {
-        type: Number,
-        default: 10,
-    },
-})
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
+});
 
 const User = models?.User || model("User", UserSchema);
 
