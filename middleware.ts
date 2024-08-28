4614520bc9c72302d37da4bf7b6cd0492bdb87d2
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-08-29 00:04:58 +0530

    webhook

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/', '/sign-up', '/api/webhooks/clerk', '/api/webhooks/(.*)', '/api/webhooks/stripe']);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
