e8e920562957dc532aec15a18b5644c21752ac2b
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-08-30 16:05:25 +0530

    commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,47 @@
-'use client';
-import { useAuth } from '@clerk/nextjs';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import React from 'react';
+import { Collection } from '@/components/shared/Collection'
+import { navLinks } from '@/constants'
+import { getAllImages } from '@/lib/actions/image.actions'
+import Image from 'next/image'
+import Link from 'next/link'
 
-const Home = () => {
-  const { isSignedIn, isLoaded } = useAuth(); 
-  const router = useRouter();
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1
+  const searchQuery = (searchParams?.query as string) || ''
 
-  useEffect(() => {
-    
-    if (isLoaded && !isSignedIn) {
-      router.push('/sign-up');
-    }
-  }, [isSignedIn, isLoaded, router]);
-
-  if (!isLoaded || !isSignedIn) {
-    return <div>Redirecting to signup...</div>;
-  }
+  const images = await getAllImages({ page, searchQuery })
 
   return (
-    <div>
-      Home
-      {/* <UserButton /> */}
-    </div>
-  );
-};
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Unleash Your Creative Vision with Imaginify
+        </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit rounded-full bg-white p-4">
+                <Image src={link.icon} alt="image" width={24} height={24} />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
 
-export default Home;
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
+      </section>
+    </>
+  )
+}
+
+export default Home
